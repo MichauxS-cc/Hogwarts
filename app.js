de08fdbec660006b190e9d315813519bfad376fe
MichauxS-cc <michaux2817@gmail.com>
2021-07-24 15:26:36 -0700

    moved build folder to root

--- app.js
+++ app.js
@@ -15,10 +15,10 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
-app.use(express.static(path.join(__dirname, "/my_hogwarts_app/build")));
+app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/my_hogwarts_app/build", "index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.use(logger("dev"));
