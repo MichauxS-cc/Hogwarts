4a0a2357c86dcb70118d5bc927bf2989d510ed26
MichauxS-cc <michaux2817@gmail.com>
2021-07-24 14:32:00 -0700

    show frontend debug 1.0

--- app.js
+++ app.js
@@ -15,13 +15,21 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "my_hogwarts_app/public")));
 app.use(cors());
 
+// app.use((req, res, next) => {
+//   res.sendFile(path.join(__dirname, "my_hogwarts_app/build", "index.html"));
+// });
 // app.use(express.static(""));
 // app.use("/db/index", indexRouter);
 // app.use("/db/db", dbRouter);
