2db461309e0922ba613551f1f32c8daf562f62f6
MichauxS-cc <michaux2817@gmail.com>
2021-07-24 17:11:07 -0700

    removed static 3000 url

--- app.js
+++ app.js
@@ -18,10 +18,6 @@ app.set("view engine", "jade");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -35,6 +31,10 @@ app.use(cors());
 // app.use("/db/", indexRouter);
 app.use("/db", dbRouter);
 // app.use("/equipDb", equipmentsRouter);
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
